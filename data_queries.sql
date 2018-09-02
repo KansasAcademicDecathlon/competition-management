e0fe9c663bed24bfa2294cd8aa482db95753b45b
Paul Vittorino <PaulVittorino@users.noreply.github.com>
2018-09-02 09:31:35 -0500

    Verify student query by school
    The schema does support querying for students only by a given school. A
    sample query demonstrates this functionality.

--- data_queries.sql
+++ data_queries.sql
@@ -4,6 +4,7 @@ https://www.w3schools.com/sql/sql_join_inner.asp */
 Select School.SchoolName, Person.FirstName, Person.LastName from Person join School on School.SchoolID=Person.SchooID where Person.CategoryID=4
 
 /* How to get a list of students for a schools */
+Select School.SchoolName, Person.FirstName, Person.LastName, Category.CategoryDescription from Person join School on School.SchoolID=Person.SchooID join Category on Category.CategoryID=Person.CategoryID where Person.CategoryID in(1,2,3) AND Person.SchooID=3
 
 /* List of all students with school, name and category */
 Select School.SchoolName, Person.FirstName, Person.LastName, Category.CategoryDescription from Person join School on School.SchoolID=Person.SchooID join Category on Category.CategoryID=Person.CategoryID where Person.CategoryID in(1,2,3)
