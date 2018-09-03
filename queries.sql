d58aabbf81ced028f64c2fc3079d32b757a2d4d3
Paul Vittorino <PaulVittorino@users.noreply.github.com>
2018-09-03 14:15:24 -0500

    Clean up queries files
    Add line breaks to the SQL queries to improve readability.

--- queries.sql
+++ queries.sql
@@ -1,10 +1,17 @@
 /* How to get the list of coaches and their schools
 https://www.w3schools.com/sql/sql_join_inner.asp */
-/* Old: Select School.SchoolName, Person.FirstName, Person.LastName from Coach inner join Person on Coach.PersonID=Person.PersonID inner join School on Person.SchoolID=School.SchoolID */
-Select School.SchoolName, Person.FirstName, Person.LastName from Person join School on School.SchoolID=Person.SchoolID where Person.CategoryID=4
+Select School.SchoolName, Person.FirstName, Person.LastName from Person
+join School on School.SchoolID=Person.SchoolID
+where Person.CategoryID=4;
 
 /* How to get a list of students for a schools */
-Select School.SchoolName, Person.FirstName, Person.LastName, Category.CategoryDescription from Person join School on School.SchoolID=Person.SchoolID join Category on Category.CategoryID=Person.CategoryID where Person.CategoryID in(1,2,3) AND Person.SchoolID=3
+Select School.SchoolName, Person.FirstName, Person.LastName, Category.CategoryDescription from Person
+join School on School.SchoolID=Person.SchoolID
+join Category on Category.CategoryID=Person.CategoryID
+where Person.CategoryID in(1,2,3) AND Person.SchoolID=3;
 
 /* List of all students with school, name and category */
-Select School.SchoolName, Person.FirstName, Person.LastName, Category.CategoryDescription from Person join School on School.SchoolID=Person.SchoolID join Category on Category.CategoryID=Person.CategoryID where Person.CategoryID in(1,2,3)
+Select School.SchoolName, Person.FirstName, Person.LastName, Category.CategoryDescription from Person
+join School on School.SchoolID=Person.SchoolID
+join Category on Category.CategoryID=Person.CategoryID
+where Person.CategoryID in(1,2,3);
