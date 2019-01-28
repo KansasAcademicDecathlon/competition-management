46ae37f2a6c80bcbdfc704ce248468eb3d45cb03
Paul Vittorino <PaulVittorino@users.noreply.github.com>
2019-01-27 23:02:05 -0600

    Add HTML roster generation
    Generate HTML files with rosters for each school.

--- sqlalchemy/person.py
+++ sqlalchemy/person.py
@@ -11,12 +11,17 @@ class Person(Base):
     PersonID = Column(Integer, primary_key=True)
     FirstName = Column(String, nullable=False)
     LastName = Column(String, nullable=False)
-    CategoryID = Column(Integer, ForeignKey('Category.CategoryID'), nullable=False)
+    CategoryID = Column(Integer, ForeignKey(
+        'Category.CategoryID'), nullable=False)
     Category = relationship("Category")
     SchoolID = Column(Integer, ForeignKey('School.SchoolID'))
     School = relationship("School")
+    StudentID = Column(Integer, unique=True)
     Email = Column(String)
 
     def __repr__(self):
         return "<Person(first='%s', last='%s', email='%s')>" % (
             self.FirstName, self.LastName, self.Email)
+
+    def FullName(self):
+        return "{0} {1}".format(self.FirstName, self.LastName)
--- sqlalchemy/reports.py
+++ sqlalchemy/reports.py
@@ -5,6 +5,8 @@ from category import Category
 from person import Person
 from school import School
 
+from html import HTML
+
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
@@ -34,6 +36,78 @@ def generate_CoachImport():
                    coach.Email, "", "", "", "www"]
             logging.debug(row)
             csvwriter.writerow(row)
+
+
+def generate_rosters():
+    """
+    Generate HTML rosters per school
+    """
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+    distutils.dir_util.mkpath("outputs")
+
+    for school in session.query(School).order_by(School.SchoolID):
+        # print school.SchoolName
+        markup = HTML()
+        head = markup.head
+        # See https://www.w3schools.com/css/tryit.asp?filename=trycss_table_striped for an example of a striped table
+        head.style(
+            """
+            table, th, td {
+                border: 2px solid black;
+                border-collapse: collapse;
+            }
+            th, td {
+                padding: 5px;
+                text-align: left;
+            }
+            tr:nth-child(even) {background-color: #f2f2f2;}""")
+        head.title(school.SchoolName)
+        body = markup.body
+
+        # Team Number
+        p = body.p
+        p.b("Team Number ")
+        p += "{:02d}".format(school.SchoolID)
+
+        # School name
+        p = body.p
+        p.b("School ")
+        p += school.SchoolName
+
+        # List out the coach(es)
+        people = session.query(Person).filter_by(
+            SchoolID=school.SchoolID).all()
+        for person in people:
+            if person.Category.CategoryDescription != "Coach":
+                continue
+            p = body.p
+            p.b("Coach ")
+            p += person.FullName()
+
+        # Table of students
+        table = body.table
+
+        table_row = table.tr
+        table_row.th("Student #")
+        table_row.th("Name")
+        table_row.th("Category")
+
+        students = session.query(Person).filter_by(
+            SchoolID=school.SchoolID).order_by(Person.StudentID).all()
+        for student in students:
+            # All participating students will have a valid StudentID
+            if student.StudentID == None:
+                continue
+            table_row = table.tr
+            table_row.td(str(student.StudentID))
+            table_row.td(student.FullName())
+            table_row.td(student.Category.CategoryDescription)
+
+        with open("outputs/"+school.SchoolName+".html", "wb") as rosterfile:
+            rosterfile.write(str(markup))
+        # print school.people
 
 
 def generate_TeamImportData():
